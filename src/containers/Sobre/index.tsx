54b4b3ca72454b22272874295b31ec72bd518f4e
Anderson Costa <costaandersom23@gmail.com>
2024-10-28 15:12:36 -0300

    estilização do "Sobre mim"

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,10 +1,15 @@
 import Title from "../../components/Title";
 import Paragrafo from "../../components/Paragrafo";
+import { GithubSection } from "./styles";
 
 const Sobre = () => (
   <section>
     <Title fontSize={16}>Sobre mim</Title>
     <Paragrafo>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam vel, tempora atque illo nam, eveniet harum sint suscipit magnam blanditiis, aliquam reprehenderit consectetur dolorum deleniti culpa! Ipsam quibusdam necessitatibus minus.</Paragrafo>
+    <GithubSection>
+    <img src="https://github-readme-stats.vercel.app/api?username=AndersonCostaDev01&show_icons=true&theme=dracula&include_all_commits=true&count_private=true"/>
+    <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=AndersonCostaDev01&layout=compact&langs_count=7&theme=dracula"/>
+    </GithubSection>
   </section>
 )
 
